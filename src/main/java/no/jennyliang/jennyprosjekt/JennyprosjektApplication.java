3b67ec8682de67f3307d44c8b69652cc758ecd78
Jenny Liang Nergård <jennyliang.nergard@skatteetaten.no>
2023-11-20 17:44:08 +0100

    Initial commit

---
+++ src/main/java/no/jennyliang/jennyprosjekt/JennyprosjektApplication.java
@@ -0,0 +1,13 @@
+package no.jennyliang.jennyprosjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JennyprosjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JennyprosjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/jennyliang/jennyprosjekt/JennyprosjektApplicationTests.java
@@ -0,0 +1,13 @@
+package no.jennyliang.jennyprosjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JennyprosjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
